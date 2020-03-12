ad754c2c833811acd9d3737794b93db1e08c322e
TheStek <sforte99@gmail.com>
2020-03-12 13:59:48 +0000

    Added graph generators

--- tests.py
+++ tests.py
@@ -1,8 +1,24 @@
 import math
 import graph
-
-x = list(range(10))
-
-print(x[1:] + [x[0]])
+import random
 
 
+def generateRandomEuclidianGraph(n, size):
+    with open(f"generated-{n}-{size}", 'w') as f:
+        for i in range(n):
+            x = random.randrange(size)
+            y = random.randrange(size)
+            f.write(f"{x} {y}\n")
+
+
+def generateRandomMetricGraph(n):
+    with open(f"generated-{n}", 'w') as f:
+        for i in range(n):
+            for j in range(n):
+                if j>i:
+                    f.write(f"{i} {j} {random.randrange(25)}\n")
+
+
+generateRandomEuclidianGraph(10, 50)
+
+generateRandomMetricGraph(6)
