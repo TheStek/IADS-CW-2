882b4069b73cfe058e0b3e2a98b497abfda6ed52
TheStek <sforte99@gmail.com>
2020-03-26 14:44:52 +0000

    Tests

--- tests.py
+++ tests.py
@@ -2,9 +2,11 @@ import math
 import graph
 import random
 import matplotlib.pyplot as plt
+import time
 
 
-def generateRandomEuclidianGraph(n, size):
+# Generate n random points in a square of given size and save to a file
+def generateRandomEuclideanGraph(n, size):
     with open(f"generated-{n}-{size}", 'w') as f:
         for i in range(n):
             x = random.randrange(size)
@@ -12,47 +14,147 @@ def generateRandomEuclidianGraph(n, size):
             f.write(f"{x} {y}\n")
 
 
-def generateRandomMetricGraph(n):
+# Generate a connected graph of n points with random edge costs and save in a file
+def generateRandomGraph(n):
     with open(f"generated-{n}", 'w') as f:
         for i in range(n):
             for j in range(n):
                 if j>i:
-                    f.write(f"{i} {j} {random.randrange(25)}\n")
+                    f.write(f"{i} {j} {random.randrange(100)}\n")
 
 
-
-def shortestArcEuclidianTest(n, size=1000, runs=10, output = True):
+# Generate a given number of random euclidean graphs with a given number of nodes in a given size
+# Calculate either the reduction in cost or runtimes with optional display
+# Display and/or return the average of these
+def shortestArcEuclideanTest(n, size=1000, runs=10, output = True, mode="reduction"):
     av = 0
 
-    print(f"Running tests using n={n}, size={size} with {runs} graphs\n")
+    if output:
+        print(f"Running tests using n={n}, size={size} with {runs} graphs\n")
 
     for i in range(runs):
-        generateRandomEuclidianGraph(n, size)
+        generateRandomEuclideanGraph(n, size)
         g = graph.Graph(-1, f"generated-{n}-{size}")
-        orig = g.tourValue()
-        g.shortestArcs()
-        new = g.tourValue()
-        reduc = 100* (orig-new) / orig
-        av += reduc
-        if output:
-            print(f"Graph {i} : {reduc}% cost reduction")
+
+        if mode == "reduction":
+            orig = g.tourValue()
+            g.shortestArcs()
+            new = g.tourValue()
+            reduc = 100* (orig-new) / orig
+            av += reduc
+            if output:
+                print(f"Graph {i} : {reduc}% cost reduction")
+
+        elif mode == "runtime":
+            start = time.time()
+            g.shortestArcs()
+            t = time.time() - start
+            if output:
+                print(f"Graph {i} : {t}s runtime")
+            av += t
+
 
     av = av/runs
 
     if output:
-        print(f"\nAverage reduction of {av}%")
+        if mode == "reduction": 
+            print(f"\nAverage reduction of {av}%\n")
+        elif mode == "runtime":
+            print(f"\nAverage runtime of {av}s\n")
 
     return av
 
-# x = [shortestArcEuclidianTest(i, output = False) for i in range(5, 120, 5)]
-
-# plt.plot(range(5, 120, 5), x)
-
-# plt.xlabel("n")
-# plt.ylabel("Average Percentage Reduction in Cost")
 
 
-# plt.show()
+# Generate a given number of random connected graphs with a given number of nodes in a given size
+# Calculate either the reduction in cost or runtimes with optional display
+# Display and/or return the average of these
+def shortestArcTest(n, runs=10, output = True, mode="reduction"):
+    av = 0
+
+    if output:
+        print(f"Running tests using n={n}, with {runs} graphs\n")
+
+    for i in range(runs):
+        generateRandomGraph(n)
+        g = graph.Graph(n, f"generated-{n}")
+
+        if mode == "reduction":
+            orig = g.tourValue()
+            g.shortestArcs()
+            new = g.tourValue()
+            reduc = 100* (orig-new) / orig
+            av += reduc
+            if output:
+                print(f"Graph {i} : {reduc}% cost reduction")
+
+        elif mode == "runtime":
+            start = time.time()
+            g.shortestArcs()
+            t = time.time() - start
+            if output:
+                print(f"Graph {i} : {t}s runtime")
+            av += t
 
 
-shortestArcEuclidianTest(100)
+    av = av/runs
+
+    if output:
+        if mode == "reduction": 
+            print(f"\nAverage reduction of {av}%")
+        elif mode == "runtime":
+            print(f"\nAverage runtime of {av}s")
+
+    return av
+
+
+# Plot either the cost reduction or runtime for random euclidean graphs of default (1000) size
+# with n in a given range and step
+def plotEuclideanGraph(start, end, step, mode="reduction"):
+    x = [shortestArcEuclideanTest(i, output = False, mode = mode) for i in range(start, end, step)]
+
+    plt.plot(range(start, end, step), x)
+
+    plt.xlabel("n")
+    if mode == "reduction":
+        plt.ylabel("Average Percentage Reduction in Cost")
+    elif mode == "runtime":
+        plt.ylabel("Average Runtime")
+
+    plt.show()
+
+
+# Plot either the cost reduction or runtime for random connected graphs of default (1000) size
+# with n in a given range and step
+def plotRandomGraph(start, end, step, mode="reduction"):
+    x = [shortestArcTest(i, output = False, mode = mode) for i in range(start, end, step)]
+
+    plt.plot(range(start, end, step), x)
+
+    plt.xlabel("n")
+    if mode == "reduction":
+        plt.ylabel("Average Percentage Reduction in Cost")
+    elif mode == "runtime":
+        plt.ylabel("Average Runtime")
+
+    plt.show()
+
+
+
+# All the tests used in the experiment section of the report
+
+shortestArcTest(100, runs=20)
+shortestArcTest(100, runs=20, mode="runtime")
+
+
+shortestArcEuclideanTest(100, runs=20)
+shortestArcEuclideanTest(100, runs=20, mode="runtime")
+
+
+plotRandomGraph(5, 120, 5)
+plotEuclideanGraph(5, 120, 5)
+
+
+plotRandomGraph(5, 120, 5, mode="runtime")
+plotEuclideanGraph(5, 120, 5, mode="runtime")
+
